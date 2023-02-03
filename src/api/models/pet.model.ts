de43fe20675fa4a83c09a81002086d4626bfdfbe
CCnova <caiocnova1@gmail.com>
2023-02-03 12:08:27 -0300

    feature: create pet model

---
+++ src/api/models/pet.model.ts
@@ -0,0 +1,22 @@
+import { ValidationError } from "../../types/errors.types";
+import { TModelValidations } from "../../types/validations.types";
+import { IIdGenerator } from "../contracts/models.contracts";
+import { IPet } from "../types";
+import { modelValidate } from "../utils";
+
+export type TCreatePetParams = Omit<IPet, "id">;
+
+const validations: Partial<TModelValidations<TCreatePetParams>> = {};
+
+export default function makePetModel(idGenerator: IIdGenerator) {
+  return {
+    validate: modelValidate<TCreatePetParams>,
+    createPet(params: TCreatePetParams): IPet | ValidationError {
+      const validationError = this.validate(params, validations);
+
+      return validationError
+        ? validationError
+        : { id: idGenerator.generate(), ...params };
+    },
+  };
+}
