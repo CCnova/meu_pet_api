1f3dafa0d827974cfcb58fbfcfcddfffd8aefc10
CCnova <caiocnova1@gmail.com>
2023-02-04 07:39:55 -0300

    chore: replace winston with bunyan

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -1,26 +1,3 @@
-import { createLogger, format, transports } from "winston";
+import * as bunyan from "bunyan";
 
-export const log = createLogger({
-  level: "info",
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: { service: "Meu-pet-API" },
-});
-
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== "production") {
-  log.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  );
-}
+export const log = bunyan.createLogger({ name: "Meu-pet-API" });
