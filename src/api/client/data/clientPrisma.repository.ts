61f1c4d35bde46ab668cca51a9306a93b0b05ae6
CCnova <caiocnova1@gmail.com>
2023-02-06 07:06:13 -0300

    feature: implement missig functions on clientPrismaRepo

--- src/api/client/data/clientPrisma.repository.ts
+++ src/api/client/data/clientPrisma.repository.ts
@@ -9,5 +9,25 @@ export default function makeClientPrismaRepository(
     insert(data: IClient) {
       return prismaClient.clientUser.create({ data });
     },
+
+    async bulkInsert(data: IClient[]) {
+      await prismaClient.clientUser.createMany({ data });
+
+      return Promise.all(
+        data.map((client) =>
+          prismaClient.clientUser.findFirstOrThrow({
+            where: { id: client.id },
+          })
+        )
+      );
+    },
+
+    async delete(id: string) {
+      const client = prismaClient.clientUser.findUnique({ where: { id } });
+
+      if (client !== null) prismaClient.clientUser.delete({ where: { id } });
+
+      return client;
+    },
   };
 }
