95ef45142ee58ab4b0b7e70599984e19a31c296c
CCnova <caiocnova1@gmail.com>
2023-03-26 10:26:10 -0300

    Change BASE_PATH variable name

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { ClientRouter } from "./client";
 
-const BASE_PATH = "/api/v1";
+const BASE_PATH_V1 = "/api/v1";
+
 const router = express.Router();
 
-ClientRouter.setup({ router, basePath: `${BASE_PATH}/client-user` });
+ClientRouter.setup({ router, basePath: `${BASE_PATH_V1}/client-user` });
 
 export default router;
